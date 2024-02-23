20d201f327ca71d8ac802b14bc1faa0cea6ffdb7
abid khan <abikhase@gmail.com>
2024-02-24 00:58:07 +0100

    toast functionality implemented

--- Angular_Spring_SQL_Project/Front-End/src/app/components/auth/signup/signup.component.ts
+++ Angular_Spring_SQL_Project/Front-End/src/app/components/auth/signup/signup.component.ts
@@ -2,11 +2,19 @@ import { Component, OnInit, inject } from '@angular/core';
 import { FormGroup, FormBuilder, Validators, ReactiveFormsModule } from '@angular/forms';
 import { AuthService } from '../../../services/auth/auth.service';
 import { HttpClientModule } from '@angular/common/http';
+import { ToastrService } from 'ngx-toastr';
+
+import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
+
+
 
 @Component({
   selector: 'app-signup',
   standalone: true,
-  imports: [ReactiveFormsModule, HttpClientModule],
+  imports: [
+    ReactiveFormsModule, 
+    HttpClientModule
+  ],
   templateUrl: './signup.component.html',
   styleUrl: './signup.component.css'
 })
@@ -14,10 +22,14 @@ export class SignupComponent implements OnInit {
 
   signupForm: FormGroup;
   
-  constructor(private fb: FormBuilder, private authService: AuthService) {}
+  constructor(
+    private fb: FormBuilder, 
+    private authService: AuthService,
+    private toastService: ToastrService
+    ) {}
 
   ngOnInit(): void {
-    this.signupForm = this.fb.group({
+    this.signupForm = this.fb.group ({
       firstName: ['', Validators.required],
       lastName: ['', Validators.required],
       email: ['', Validators.required],
@@ -50,8 +62,14 @@ export class SignupComponent implements OnInit {
 
           this.authService.signup(this.signupForm.value)
           .subscribe( {
-            next: (res)   => { console.log("Response: ", res) },
-            error: (err)  => {console.log("Error: ", err)},
+            next: (res)   => { 
+              console.log("Response: ", res) 
+              this.toastService.success('Registration successful!', 'Success');
+            },
+            error: (err)  => {
+              console.log("Error: ", err)
+              this.toastService.error("User already exists", "Error")
+            },
             complete: ()  => console.log("Exerything looks good")
           })
 
